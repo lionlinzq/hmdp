a0cfe69d8d566ea140897a3125e4b0f9ba360fcb
lionlinzq <niodindeco@hotmail.com>
2023-05-15 23:40:22 +0800

    提交一下

--- src/main/java/com/hmdp/dto/LoginFormDTO.java
+++ src/main/java/com/hmdp/dto/LoginFormDTO.java
@@ -2,9 +2,16 @@ package com.hmdp.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class LoginFormDTO {
+    @NotBlank(message = "手机号码不能为空")
+    @Pattern(regexp = "^[1][3,4,5,6,7,8,9][0-9]{9}$", message = "手机号格式有误")
     private String phone;
+
     private String code;
+
     private String password;
 }
--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexPatterns;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * <p>
@@ -15,6 +23,37 @@ import org.springframework.stereotype.Service;
  * @since 2021-12-22
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        // 1.校验手机号
+        boolean phoneInvalid = RegexUtils.isPhoneInvalid(phone);
+        if (phoneInvalid) {
+            return Result.fail("手机号格式错误！");
+        }
+        // 2、生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        // 3、保存到session
+        session.setAttribute("code",code);
+
+        // 4、发送验证码
+        log.debug("发送验证码成功，验证码：{}",code);
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+
+        Object code = session.getAttribute("code");
+        String loginFormCode = loginForm.getCode();
+        if (code == null || !loginFormCode.toString().equals(code)){
+            return Result.fail("验证码错误");
+        }
+        log.debug("登录成功");
+        return Result.ok();
+    }
 }
